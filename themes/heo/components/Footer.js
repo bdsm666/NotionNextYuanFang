8d9b3ec4ea92e99cd4de3ad242d328d8cec3af3e
bdsm666 <pysm0393@gmail.com>
2026-04-10 13:22:20 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
   const BEI_AN_LINK = siteConfig('BEI_AN_LINK')
   const BIO = siteConfig('BIO')
   return (
-    <footer className='relative flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm'>
+    <footer className='relative bg-white text-gray-500 text-sm p-6 text-center flex flex-col items-center justify-center w-full'>
       {/* 颜色过度区 */}
       <div
         id='color-transition'
